3454b9f0f3dd54e6a5351a167cc1a7aa6d76d8b4
kbburk-TSF <kevin@temporalinertia.com>
2026-01-18 15:16:14 -0600

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,20 @@
 import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import { registerComponent } from "@plasmicapp/react-web/lib/host";
 import Head from "next/head";
 import Link from "next/link";
+import { WalmartDashboard } from "../components/WalmartDashboard";
+
+// Register the Walmart Dashboard component for Plasmic
+registerComponent(WalmartDashboard, {
+  name: "WalmartDashboard",
+  props: {
+    apiBase: {
+      type: "string",
+      defaultValue: "https://tsf-demand-back.onrender.com/api/walmart"
+    }
+  }
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
